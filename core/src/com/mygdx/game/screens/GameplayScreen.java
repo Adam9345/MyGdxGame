874a49ac2f00c8e8ef5149947853951352245ac2
Adam9345 <klonica3a@gmail.com>
2018-12-09 16:49:45 +0100

    added dummy GameplayScreen

---
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.MyGdxGame;
+
+public class GameplayScreen extends AbstractScreen {
+    public GameplayScreen(MyGdxGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+    }
+}
