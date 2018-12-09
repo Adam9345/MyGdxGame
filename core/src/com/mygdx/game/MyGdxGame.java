e09540a4c405b4fb1fa00ee7778de62eb069174d
Adam9345 <klonica3a@gmail.com>
2018-12-09 20:10:13 +0100

    reset player score

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,9 +3,6 @@ package com.mygdx.game;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Preferences;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.screens.SplashScreen;
 
 public class MyGdxGame extends Game {
@@ -22,33 +19,42 @@ public class MyGdxGame extends Game {
     private int points;
 
     private Preferences prefs;
-	
-	@Override
-	public void create () {
-	    init();
-	    this.setScreen(new SplashScreen(this));
-	}
+
+    @Override
+    public void create() {
+        init();
+        this.setScreen(new SplashScreen(this));
+    }
 
     private void init() {
-	    prefs = Gdx.app.getPreferences(GAME_PREFS);
-	    loadScore();
+        prefs = Gdx.app.getPreferences(GAME_PREFS);
+        loadScore();
     }
 
     private void loadScore() {
         points = prefs.getInteger(GAME_SCORE);
-	}
-
-    public void addPoint() {
-	    points += 1;
-	    prefs.putInteger(GAME_SCORE, points);
-	    prefs.flush();
     }
 
-	/*
-	 *
-	 * getters and setters
-	 *
-	 */
+    public void addPoint() {
+        points += 1;
+        updateSavedScoreInPrefs();
+    }
+
+    public void resetGameScore() {
+        points = 0;
+        updateSavedScoreInPrefs();
+    }
+
+    private void updateSavedScoreInPrefs() {
+        prefs.putInteger(GAME_SCORE, points);
+        prefs.flush();
+    }
+
+    /*
+     *
+     * getters and setters
+     *
+     */
 
     public boolean isPaused() {
         return paused;
--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -14,6 +14,7 @@ public class GameplayScreen extends AbstractScreen {
 
     private Player player;
     private Button playerButton;
+    private Button resetScoreButton;
     private Label scoreLabel;
 
     public GameplayScreen(MyGdxGame game) {
@@ -24,7 +25,27 @@ public class GameplayScreen extends AbstractScreen {
     protected void init() {
         initPlayer();
         initPlayerButton();
+        initResetScoreLabel();
         initScoreLabel();
+    }
+
+    private void initResetScoreLabel() {
+        resetScoreButton = new Button(new ButtonStyle());
+        resetScoreButton.setWidth(100);
+        resetScoreButton.setHeight(100);
+        resetScoreButton.setX(330);
+        resetScoreButton.setY(550);
+        resetScoreButton.setDebug(true);
+
+        stage.addActor(resetScoreButton);
+
+        resetScoreButton.addListener(new ClickListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                game.resetGameScore();
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
     }
 
     private void initScoreLabel() {
