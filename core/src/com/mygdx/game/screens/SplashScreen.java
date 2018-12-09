0d927fdb604356dd142efc51f17c24c934ebccbe
Adam9345 <klonica3a@gmail.com>
2018-12-09 14:28:44 +0100

    added AbstractScreen and SplashScreen

---
+++ core/src/com/mygdx/game/screens/SplashScreen.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.screens;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.mygdx.game.MyGdxGame;
+
+public class SplashScreen extends AbstractScreen {
+
+    private Texture splashImg;
+
+    public SplashScreen(MyGdxGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {
+        // TODO implement better assets loading when game grows
+        splashImg = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+
+        spriteBatch.begin();
+        spriteBatch.draw(splashImg, 0, 0);
+        spriteBatch.end();
+    }
+}
