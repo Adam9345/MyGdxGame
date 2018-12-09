c635d0f3ddc84644fa317f167e8e560df621624c
Adam9345 <klonica3a@gmail.com>
2018-12-09 19:10:49 +0100

    added score label to GameplayScreen and removed debug that counts points

--- core/src/com/mygdx/game/screens/GameplayScreen.java
+++ core/src/com/mygdx/game/screens/GameplayScreen.java
@@ -1,8 +1,11 @@
 package com.mygdx.game.screens;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.mygdx.game.MyGdxGame;
 import com.mygdx.game.entities.Player;
@@ -11,6 +14,7 @@ public class GameplayScreen extends AbstractScreen {
 
     private Player player;
     private Button playerButton;
+    private Label scoreLabel;
 
     public GameplayScreen(MyGdxGame game) {
         super(game);
@@ -20,6 +24,16 @@ public class GameplayScreen extends AbstractScreen {
     protected void init() {
         initPlayer();
         initPlayerButton();
+        initScoreLabel();
+    }
+
+    private void initScoreLabel() {
+        LabelStyle labelStyle = new LabelStyle();
+        labelStyle.font = new BitmapFont();
+        scoreLabel = new Label("", labelStyle);
+        scoreLabel.setX(20);
+        scoreLabel.setY(650);
+        stage.addActor(scoreLabel);
     }
 
     private void initPlayerButton() {
@@ -51,8 +65,6 @@ public class GameplayScreen extends AbstractScreen {
     public void render(float delta) {
         super.render(delta);
         update();
-
-        System.out.println("Points: " + game.getPoints());
 
         spriteBatch.begin();
         stage.draw();
